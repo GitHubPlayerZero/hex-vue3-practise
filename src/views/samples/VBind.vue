26489b9a9c32c28868f5a8a685e9eab0db42773c
GitHubPlayer0 <65072506+GitHubPlayerZero@users.noreply.github.com>
2024-08-06 15:07:43 +0800

    增加範例 3、範例 4、動態陣列範例

--- src/views/samples/VBind.vue
+++ src/views/samples/VBind.vue
@@ -43,6 +43,44 @@
 		&nbsp;
 		<input type="text" :disabled="isDisabled" />
 	</fieldset>
+	
+	<fieldset class="fieldset mb-24">
+		<legend>範例 3</legend>
+		
+		<p>
+			綁定固定值。<br>
+			只能使用字串，需要使用單引號／雙引號／樣板字面值將其包起來。
+		</p>
+		
+		<label class="box mb-24">
+			<input type="checkbox">&nbsp;&nbsp;一般 HTML 寫法
+		</label>
+		
+		<!-- [Vue warn]: Property "checkbox" was accessed during render but is not defined on instance. -->
+		<label class="box mb-24">
+			<input :type="checkbox">
+			<br>
+			<p class="mt-3">
+				使用 v-bind 綁定 type，但是其值 (checkbox) 沒有使用字串形式，所以 checkbox 會被視為 vue 資料，而在 console 出現渲染錯誤的警告。<br>
+				這邊也無法正常顯示為 checkbox。
+			</p>
+		</label>
+		
+		<label class="box">
+			<input :type="'checkbox'">&nbsp;&nbsp;
+			使用 v-bind 綁定 type，其值 (checkbox) 使用單引號包覆代表字串形式。
+		</label>
+		
+	</fieldset>
+	
+	<fieldset class="fieldset mb-24">
+		<legend>範例 4</legend>
+		<p>以物件方式綁定，可以一次綁定多個屬性。</p>
+		<label>
+			<input v-bind="{ type: 'checkbox', id: 'chk1', name: 'chkgroup1', test: 'aaa', style: 'accent-color: red; width: 50px;'}">&nbsp;&nbsp;
+			按我
+		</label>
+	</fieldset>
 
 	<hr class="hr" />
 
@@ -71,9 +109,19 @@
 		<legend>搭配三元運算子判斷</legend>
 
 		<div class="mb-32">
-			<label> <input type="checkbox" v-model="isRotate" />&nbsp; 旋轉 </label>
+			<label>
+				<input type="checkbox" v-model="isRotate" />&nbsp;
+				旋轉&nbsp;
+				<span>isRotate：{{ isRotate }}</span>
+			</label>
+			
 			&nbsp;&nbsp;&nbsp;
-			<label> <input type="checkbox" v-model="isChangeColor" />&nbsp; 變色 </label>
+			
+			<label>
+				<input type="checkbox" v-model="isChangeColor" />&nbsp;
+				變色&nbsp;
+				<span>isChangeColor：{{ isChangeColor }}</span>
+			</label>
 		</div>
 
 		<div>
@@ -103,9 +151,19 @@
 		<legend>物件寫法</legend>
 
 		<div class="mb-32">
-			<label><input type="checkbox" v-model="isRotate2" /> 旋轉</label
-			>&nbsp;&nbsp;&nbsp;&nbsp;
-			<label><input type="checkbox" v-model="isChangeColor2" /> 變色</label>
+			<label>
+				<input type="checkbox" v-model="isRotate2" />&nbsp;
+				旋轉&nbsp;
+				<span>isRotate2：{{ isRotate2 }}</span>
+			</label>
+			
+			&nbsp;&nbsp;&nbsp;&nbsp;
+			
+			<label>
+				<input type="checkbox" v-model="isChangeColor2" />&nbsp;
+				變色&nbsp;
+				<span>isChangeColor2：{{ isChangeColor2 }}</span>
+			</label>
 		</div>
 
 		<div :class="{ 'box-sample': true, rotate: isRotate2, 'bg-green': isChangeColor2 }"></div>
@@ -114,6 +172,20 @@
 	<fieldset class="fieldset mb-40 pl-28 pb-28">
 		<legend class="mb-16">陣列寫法</legend>
 		<div :class="['box-sample', 'rotate', 'bg-green']"></div>
+		
+		<div class="box mt-5 pl-32 pb-32">
+			<p>動態陣列</p>
+			
+			<div class="mb-32">
+				<label><input type="checkbox" v-model="classList" value="rotate" /> 旋轉</label>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<label><input type="checkbox" v-model="classList" value="bg-green" /> 變色</label>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<span>classList：{{ classList }}</span>
+			</div>
+			
+			<div class="box-sample" :class="classList"></div>
+		</div>
 	</fieldset>
 </template>
 
@@ -135,6 +207,7 @@ const isRotate = ref(false);
 const isChangeColor = ref(false);
 const isRotate2 = ref(false);
 const isChangeColor2 = ref(false);
+const classList = ref([]);
 </script>
 
 <style lang="scss" scoped>
